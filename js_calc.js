9fdb3f77723c3e5f976d93866bf5a9752479b772
wensquared <71566436+wensquared@users.noreply.github.com>
2021-06-09 22:38:09 +0200

    refined input|added missing btns

--- js_calc.js
+++ js_calc.js
@@ -1,22 +1,12 @@
-//regex:
-// while string regex is not 'number operator number =' or 'number operator number operator'
-//     if operator && string is empty
-//         hint: type number first pls
-//         break;
-//     if number && string is empty
-//         string += number;
-//     else
-//         string += number/operator
+/*
+TODO: 
+    typing new number after = , still saved the last result, only save of typing an
+    operator
+    
+    keypress functionality
 
-
-// if string regex 'number operator number ='
-//     remove = 
-//     result = calculate num op num
-//     string = result;
-// else (number operator number operator)
-//     tmp =  operator 
-//     result = calculate num op num
-//     string = result operator;
+    design
+*/
 
 function calculate(val1,val2,operator){
     let result = 0;
@@ -45,7 +35,14 @@ function calculate(val1,val2,operator){
 
     return result;
 }
-function seperate(value){
+
+function remove_children(parent) {
+    while(parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function seperate(value,lastOperator){
     
     let result = 0;
     let operator = '';
@@ -71,36 +68,97 @@ function seperate(value){
         result = calculate(first,second1,operator);
     }
 
-
+    let m = document.createElement('span');
+    m.textContent = result;
     // console.log(first);
     // console.log(second1);
     // console.log(operator);
-
+    if (lastOperator == '=') {
+        onScreenExp = result.toString();
+        remove_children(results);
+        remove_children(typed);
+        
+    } else {
+        onScreenExp = result.toString() + lastOperator;
+        //console.log('bei andern:' + onScreenExp);
+        remove_children(results);
+        remove_children(typed);
+        typed.appendChild(m);
+        let lastop = document.createElement('span')
+        lastop.textContent = lastOperator;
+        typed.appendChild(lastop);
+    }
+    
+    results.appendChild(m);
+    console.log('Last: ' + onScreenExp);
     console.log(result);
     
 }
+function checking(number){
+    let regex = /^[\-]?\d+(\.\d+)?[\+\-\*\/]{1}[\+\-]?\d+(\.\d+)?[\+\-\*\/\=]{1}$/
 
-let onScreenExp = '-3.5--3=';
+    let isNum = /\d/;
+    let isOp = /[\+\-\*\/]/;
 
-let regex = /^[\-]?\d+(\.\d+)?[\+\-\*\/]{1}[\+\-]?\d+(\.\d+)?[\+\-\*\/\=]{1}$/
 
-if (regex.test(onScreenExp)) {
-    let lastOperator = onScreenExp.slice(-1);
-    seperate(onScreenExp.slice(0,-1));
-} else {
-    console.log('not valid')
+    let p = document.createElement('span');
+    p.textContent = number;
+    typed.appendChild(p);
+    console.log('Len: ' + onScreenExp.length);
+    console.log(results.hasChildNodes() + ' ' + typed.hasChildNodes());
+    if((onScreenExp.length == 0)){
+        onScreenExp = number.toString();
+    }
+    else if ((typed.hasChildNodes() == true) && (results.hasChildNodes() == true)) {
+        console.log('hierher');
+        onScreenExp = number.toString();
+    }
+    else{
+        onScreenExp += number.toString();
+    }
+    //|| i 
+    console.log(onScreenExp);
+
+    if (regex.test(onScreenExp)) {
+        let lastOperator = onScreenExp.slice(-1);
+        //console.log(lastOperator);
+        seperate(onScreenExp.slice(0,-1),lastOperator);
+    } else {
+        console.log('not valid')
+    }
 }
+
 
 // function textAppear(number){
 //     let p = document.createElement('span');
 //     p.textContent = number;
-//     result.appendChild(p);
-//     onScreenStr += number.toString();
-    
+//     typed.appendChild(p);
+//     onScreenExp += number.toString();
+//     console.log(onScreenExp);
 // }
 
-// btn1.addEventListener('click',function(){textAppear(1)})
-// btn2.addEventListener('click',function(){textAppear(2)})
-// btnplus.addEventListener('click',function(){textAppear('+')})
-// btnplus.addEventListener('click',function(){textAppear('-')})
-// btnresult.addEventListener('click',function(){operate()})
+let onScreenExp = '';
+//console.log(onScreenExp);
+
+btn1.addEventListener('click',function(){checking(1)})
+btn2.addEventListener('click',function(){//textAppear(2);
+    checking(2)})
+btn3.addEventListener('click',function(){checking(3)})
+btn4.addEventListener('click',function(){checking(4)})
+btn5.addEventListener('click',function(){checking(5)})
+btn6.addEventListener('click',function(){checking(6)})
+btn7.addEventListener('click',function(){checking(7)})
+btn8.addEventListener('click',function(){checking(8)})
+btn9.addEventListener('click',function(){checking(9)})
+btn0.addEventListener('click',function(){checking(0)})
+btndot.addEventListener('click',function(){checking('.')})
+
+
+btnplus.addEventListener('click',function(){//textAppear('+');
+checking('+')})
+btnminus.addEventListener('click',function(){//textAppear('-');
+checking('-')})
+btnmul.addEventListener('click',function(){checking('*')})
+btndiv.addEventListener('click',function(){checking('/')})
+btnresult.addEventListener('click',function(){//textAppear('=');
+checking('=')})
